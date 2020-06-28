b7249bd8dbd18ff88cb6c147cecbfbc95487cbf7
my_ee <yb_coder@163.com>
2020-06-28 10:19:16 +0800

    这个是第二次提交

---
+++ src/main/java/com/hcycom/sso/domain/Student.java
@@ -0,0 +1,36 @@
+package com.hcycom.sso.domain;
+
+public class Student {
+
+
+    private String name;
+
+    private String address;
+
+    private Integer integer;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getInteger() {
+        return integer;
+    }
+
+    public void setInteger(Integer integer) {
+        this.integer = integer;
+    }
+}
